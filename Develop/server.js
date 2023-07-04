f0d3d3ee17f12543f66db96197688c7a1a4bac05
mkidee <mckaymemmott@gmail.com>
2023-07-03 20:33:08 -0600

    created json notes route, added needed ;

--- Develop/server.js
+++ Develop/server.js
@@ -16,8 +16,13 @@ app.use(express.static('public'));
 //request handling incoming:
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
-})
+});
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
-})
+});
+
+//brings in JSON notes db
+app.get('/api/notes', (req, res) => {
+    res.json(notesDB);
+});
