dc2d6b0e802a087a443956ea4daf305135ed8fde
mkidee <mckaymemmott@gmail.com>
2023-07-03 20:21:35 -0600

    prepped express for data parse

--- Develop/server.js
+++ Develop/server.js
@@ -4,6 +4,13 @@ const fs = require('fs');
 const path = require('path');
 const notesDB = require('./db/db.json');
 
+//need express, express instance 
 const express = require('express');
-
 const app = express();
+
+//this code is for express to handle data parsing
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+
+
