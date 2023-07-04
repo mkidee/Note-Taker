b1a206225afefec5db6c54d81990f0807972f4c5
mkidee <mckaymemmott@gmail.com>
2023-07-03 20:24:12 -0600

    created routes for index and notes page

--- Develop/server.js
+++ Develop/server.js
@@ -13,4 +13,11 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
 
+//request handling incoming:
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+})
