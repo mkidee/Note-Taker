79c2065953f0ca521b793eedc44c688b177d9ba4
mkidee <mckaymemmott@gmail.com>
2023-07-03 20:37:58 -0600

    created a route incase no endpoint is specified

--- Develop/server.js
+++ Develop/server.js
@@ -26,3 +26,8 @@ app.get('/notes', (req, res) => {
 app.get('/api/notes', (req, res) => {
     res.json(notesDB);
 });
+
+//to handle unspecified endpoints:
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '.public/index.html'));
+});
